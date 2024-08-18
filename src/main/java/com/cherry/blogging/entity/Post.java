3c8ea950c38663064ecf66e9eda6c4aa7e802c53
theLeviAckerman <armandliK@outlook.com>
2024-08-18 21:15:29 +0530

    Create Post.java

---
+++ src/main/java/com/cherry/blogging/entity/Post.java
@@ -0,0 +1,36 @@
+package com.cherry.blogging.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "post_title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "post_content", nullable = false, length = 1024)
+    private String content;
+
+    private  String imageName;
+
+    private Date addedDate;
+
+
+
+
+
+}
