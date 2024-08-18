d529a27e0fc23ab344772cc7572187a8921ad594
theLeviAckerman <armandliK@outlook.com>
2024-08-18 21:10:41 +0530

    Updated CategoryDto for validations.

--- src/main/java/com/cherry/blogging/dto/CategoryDto.java
+++ src/main/java/com/cherry/blogging/dto/CategoryDto.java
@@ -17,10 +17,11 @@ public class CategoryDto {
 
     private int id;
 
-    @NotEmpty
+    @NotEmpty(message = "Category Title Should be between 3 to 64 Characters" )
     @Size(min = 3,max = 64,message = "Category Title Should be between 3 to 64 Characters")
     private String title;
-    @NotEmpty
+
+    @NotEmpty(message = "Category Description Should be between 10 to 1024 Characters")
     @Size(min = 3,max = 1024,message = "Category Description Should be between 10 to 1024 Characters")
     private String description;
 }
